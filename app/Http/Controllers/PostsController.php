4d172cb8b5a9322495fa0d882a6e6bbcdaf8667f
Lloople <hello@davidllop.com>
2018-07-10 21:13:12 +0200

    Add pagination inside posts for previous and next entry. Fixes #1
    Also, changed < > pagination icons to emojis to be more user-friendly, and because I like emojis. utf8mb4 came to stay 🤘

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,15 +9,19 @@ class PostsController
 {
     public function show(string $slug, Sheets $sheets): View
     {
-        $post = $sheets->collection('posts')->all()
-            ->where('slug', $slug)
-            ->first();
+        $posts = $sheets->all()->sortByDesc('date');
 
-        if ($post === null) {
+        $postKey = $posts->search(function ($post) use ($slug) {
+            return $post->slug === $slug;
+        });
+
+        if ($postKey === false) {
             abort(404);
         }
 
         return view('post')
-            ->with('post', $post);
+            ->with('post', $posts->get($postKey))
+            ->with('previousPost', $posts->get($postKey+1))
+            ->with('nextPost', $posts->get($postKey-1));
     }
 }
--- resources/views/_partials/pagination.blade.php
+++ resources/views/_partials/pagination.blade.php
@@ -1,30 +1,18 @@
-<ul class="flex justify-center | mb-12 | list-reset">
+<ul class="flex justify-around | mb-12 | list-reset">
     <li class="px-1">
-        @if($elements->currentPage() === 1)
-            <span class="text-2xl text-orange-light">
-                &lt; previous
-            </span>
-        @else
-            <a
-                href="{{ route('home', ['page' => $elements->currentPage() - 1]) }}"
-                class="text-2xl text-orange no-underline"
-            >
-                &lt; previous
+        @if($elements->currentPage() !== 1)
+            <a href="{{ route('home', ['page' => $elements->currentPage() - 1]) }}"
+               class="text-2xl text-orange no-underline">
+                👈 previous
             </a>
         @endif
     </li>
-
+    
     <li class="px-1">
-        @if($elements->currentPage() === $elements->lastPage())
-            <span class="text-2xl text-orange-light">
-                next &gt;
-            </span>
-        @else
-            <a
-                href="{{ route('home', ['page' => $elements->currentPage() + 1]) }}"
-                class="text-2xl text-orange no-underline"
-            >
-                next &gt;
+        @if($elements->currentPage() !== $elements->lastPage())
+            <a href="{{ route('home', ['page' => $elements->currentPage() + 1]) }}"
+               class="text-2xl text-orange no-underline">
+                next 👉
             </a>
         @endif
     </li>
