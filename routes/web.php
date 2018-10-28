d616ec74dafb40f2b5153ea631dfa71d25ba81f2
David Llop <d.lloople@gmail.com>
2018-10-28 22:35:06 +0100

    Rename laravel multitenancy jobs post and create redirect

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 Route::feeds();
 
+Route::redirect('make-your-laravel-jobs-multi-tenancy-friendly', 'laravel-multitenancy-jobs');
+
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/about', 'AboutController@index')->name('about');
 Route::get('/{slug}', 'PostsController@show')->name('post');
