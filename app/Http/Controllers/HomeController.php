871b196f964e27bbc97711967a7e9afdd5efcd49
Lloople <hello@davidllop.com>
2018-11-17 23:32:11 +0100

    Add published flag to posts

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,8 +7,15 @@ use Spatie\Sheets\Sheets;
 
 class HomeController
 {
+
     public function index(Sheets $sheets): View
     {
-        return view('home')->with('posts', $sheets->all()->sortByDesc('date')->paginate(3));
+        $posts = $sheets
+            ->all()
+            ->where('published', true)
+            ->sortByDesc('date')
+            ->paginate(3);
+
+        return view('home')->with('posts', $posts);
     }
 }
