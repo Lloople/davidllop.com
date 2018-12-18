0f8e9ebeda4767ffe103cdd95f1e6708acac0270
David Llop <d.lloople@gmail.com>
2018-12-18 22:45:07 +0100

    Modify some alts

--- resources/views/_partials/sorcerers.blade.php
+++ resources/views/_partials/sorcerers.blade.php
@@ -1,4 +1,4 @@
 <div class="sorcerers text-center">
-    <img class="w-8" style="-webkit-transform: scaleX(-1);transform: scaleX(-1);" src="/media/simon-the-sorcerer-calypso.gif" alt="Be careful with that!">
-    <img class="w-12" src="/media/simon-the-sorcerer.gif" alt="Let's do some magic 🙄">
+    <img class="w-8" style="-webkit-transform: scaleX(-1);transform: scaleX(-1);" src="/media/simon-the-sorcerer-calypso.gif" alt="Calypso: Be careful with that!">
+    <img class="w-12" src="/media/simon-the-sorcerer.gif" alt="Simon: Let's do some magic 🙄">
 </div>
