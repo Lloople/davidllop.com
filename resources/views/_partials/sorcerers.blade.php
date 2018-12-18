2e55210800c29b6cd74c38e8876a98f84a8590af
Lloople <hello@davidllop.com>
2018-12-18 13:51:36 +0100

    Include sorcerers in about page too. They're pretty cool

---
+++ resources/views/_partials/sorcerers.blade.php
@@ -0,0 +1,4 @@
+<div class="sorcerers">
+    <img style="float: right;" src="/media/simon-the-sorcerer.gif" alt="Doing some magic 🙄">
+    <img style="float: right;-webkit-transform: scaleX(-1);transform: scaleX(-1);" src="/media/simon-the-sorcerer-calypso.gif" alt="Be careful with that!">
+</div>
