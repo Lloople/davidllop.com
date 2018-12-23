07639fcdadaed185d2480b9b43537346259bdfbd
Lloople <hello@davidllop.com>
2018-12-23 20:00:43 +0100

    Added the Calpyso Magicke Emporium music to the Simon's easter egg

--- public/js/sorcerers.js
+++ public/js/sorcerers.js
@@ -1,7 +1,9 @@
 let conversationStarted = false;
 let conversation = 1;
-const conversations =  document.querySelectorAll('.conversations .conversation');
+const conversations = document.querySelectorAll('.conversations .conversation');
+let audio = null;
 
+// Show the next conversation when clicking on Simon
 document.querySelector('#simon').addEventListener('click', async (e) => {
     e.preventDefault();
 
@@ -10,6 +12,27 @@ document.querySelector('#simon').addEventListener('click', async (e) => {
     if (conversation < conversations.length) {
         conversation++;
     }
+});
+
+// Load the music after clicking in the Sorcerers area
+document.querySelector('.sorcerers').addEventListener('click', () => {
+
+    // Just one audio element per request, I don't want you to download the music if you don't click on the Sorcerers!
+    if (audio === null) {
+        audio = new Audio('/media/calypso_magicke_emporium.mp3');
+        audio.loop = true;
+        audio.play();
+
+        // Only play music when the mouse is inside the Sorcerers area
+        document.querySelector('.sorcerers').addEventListener('mouseover', () => {
+            audio.volume = 0.7;
+        });
+    }
+});
+
+// Mute the music when the mouse leaves the Sorcerers area
+document.querySelector('.sorcerers').addEventListener('mouseleave', (e) => {
+    audio.volume = 0.0;
 });
 
 let startConversation = async () => {
