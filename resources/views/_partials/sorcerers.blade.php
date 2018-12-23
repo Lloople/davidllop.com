556b8d148c174b24ada78d4d18146491fd972146
David Llop <d.lloople@gmail.com>
2018-12-23 20:00:19 +0100

    Improved conversations flow on the Simon's easter egg

--- resources/views/_partials/sorcerers.blade.php
+++ resources/views/_partials/sorcerers.blade.php
@@ -1,27 +1,27 @@
 <div class="sorcerers text-right xs:text-center">
     <div class="conversations">
         <div class="conversation xs:text-center">
-            <div data-wait="3000" class="cbbl -right">Got any hints?</div>
-            <div data-wait="3000" class="cbbl">No, I'm afraid not.</div>
+            <div data-wait="1500" class="cbbl -right">Got any hints?</div>
+            <div data-wait="1000" class="cbbl">No, I'm afraid not.</div>
             <div data-wait="3000" class="cbbl">However if you search within yourself, then...</div>
-            <div data-wait="2000" class="cbbl -right">Cut it out, ok?</div>
-            <div data-wait="5000" class="cbbl -right">I asked for a hint not the crazy philosophies of a Father Christmas look-a-like.</div>
-            <div data-wait="4000" class="cbbl">Heed my words, bold adventurer, and your path will lead to wisdom and..</div>
-            <div data-wait="4000" class="cbbl -right">There's just no reasoning with him...</div>
+            <div data-wait="1500" class="cbbl -right">Cut it out, ok?</div>
+            <div data-wait="4000" class="cbbl -right">I asked for a hint not the crazy philosophies of a Father Christmas look-a-like.</div>
+            <div data-wait="3000" class="cbbl">Heed my words, bold adventurer, and your path will lead to wisdom and..</div>
+            <div data-wait="2500" class="cbbl -right">There's just no reasoning with him...</div>
         </div>
 
         <div class="conversation xs:text-center">
-            <div data-wait="3000" class="cbbl -right">I'm totally stuck as what to do.</div>
-            <div data-wait="6000" class="cbbl">I really wish I could help you but I'm just a clueless old fool.</div>
-            <div data-wait="3000" class="cbbl -right">Never a truer word was spoken.</div>
+            <div data-wait="2500" class="cbbl -right">I'm totally stuck as what to do.</div>
+            <div data-wait="3000" class="cbbl">I really wish I could help you but I'm just a clueless old fool.</div>
+            <div data-wait="1500" class="cbbl -right">Never a truer word was spoken.</div>
         </div>
 
         <div class="conversation xs:text-center">
-            <div data-wait="3000" class="cbbl -right">Still clueless?</div>
-            <div data-wait="3000" class="cbbl">I'm afraid so.</div>
-            <div data-wait="3000" class="cbbl -right">Still look like a Father Christmas?</div>
-            <div data-wait="3000" class="cbbl">I'm afraid so.</div>
-            <div data-wait="3000" class="cbbl -right">Good.</div>
+            <div data-wait="1000" class="cbbl -right">Still clueless?</div>
+            <div data-wait="1000" class="cbbl">I'm afraid so.</div>
+            <div data-wait="1500" class="cbbl -right">Still look like a Father Christmas?</div>
+            <div data-wait="1000" class="cbbl">I'm afraid so.</div>
+            <div data-wait="1000" class="cbbl -right">Good.</div>
         </div>
     </div>
 
